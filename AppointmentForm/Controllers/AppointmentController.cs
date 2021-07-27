12597eea65c27581fec8ced305dffebc8fd61f78
Simeon Ayara <simeondigital@gmail.com>
2021-07-27 09:02:55 +0100

    Appointment form project

---
+++ AppointmentForm/Controllers/AppointmentController.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentForm.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(AppointmentModel Appointment )
+        {
+            return View("Details", Appointment);
+        }
+    }
+}
---
+++ AppointmentForm/Models/AppointmentModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentForm.Models
+{
+    public class AppointmentModel
+    {
+        public int Id { get; set; }
+        [StringLength(25, MinimumLength =4)]
+        [Required]
+        [DisplayName("Patient Name ") ]
+        public string PatientName { get; set; }
+        [DisplayName("Patient Address")]
+        public string Street { get; set; }
+        [DisplayName("City")]
+        public String City { get; set; }
+        [Required]
+        [DisplayName("Email Address")]
+        public String Email { get; set; }
+        [Required]
+        [DisplayName("Phone Number")]
+        public int Phone { get; set; }
+        [DisplayName("Net Worth")]
+        [DataType(DataType.Currency)]
+        [Range(150000,9999999999999)]
+        public decimal Networth { get; set; }
+        [DisplayName("Patient level of Pain")]
+        [Range(1,10)]
+        public int PainLevel { get; set; }
+        [Required]
+        public String DoctorsName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+       
+    }
+}
